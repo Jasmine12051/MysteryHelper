8200b63d6cab977a60aa6f83884bc0ee26ec7e3f
Jasmine Allen <jaguilar@stu.jsu.edu>
2024-01-23 14:13:15 -0600

    Added string array, GetResource(), and started the onClick.

--- app/src/main/java/com/example/lab01a/MainActivity.java
+++ app/src/main/java/com/example/lab01a/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.lab01a;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.lab01a.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -14,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        String[] storySelection = getResources().getStringArray(R.array.story_selection);
+
+        Log.d("LENGTH", "StorySelection: " + (int)Math.random()*22 + 1);
+
+        binding.generatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EditText input = binding.inputField;
+                TextView t = binding.output;
+                //String text = input.getText().toString();
+                //t.setText(text);
+            }
+        });
     }
 }
